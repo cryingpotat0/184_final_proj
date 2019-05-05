26e21c8b56e1993767421313a4de4b3ad3abd9c3
Dylan Rowe <dylanrowe@berkeley.edu>
2019-05-05 10:57:19 -0700

    curved ray bbox 'maybe'

--- src/bbox.cpp
+++ src/bbox.cpp
@@ -16,36 +16,80 @@ bool BBox::intersect(const Ray& r, double& t0, double& t1) const {
   // If the ray intersected the bouding box within the range given by
   // t0, t1, update t0 and t1 with the new intersection times.
 
-  double
-          t_min_x = (min.x - r.o.x) / r.d.x,
-          t_max_x = (max.x - r.o.x) / r.d.x;
+  if (r.curved) {
+	  double t_min_x_1 = (r.b.x + sqrt(r.b.x*r.b.x - 4 * r.a.x * (r.c.x + min.x))) / (2.0 * r.a.x);
+	  double t_min_x_2 = (r.b.x - sqrt(r.b.x*r.b.x - 4 * r.a.x * (r.c.x + min.x))) / (2.0 * r.a.x);
+	  double t_max_x_1 = (r.b.x + sqrt(r.b.x*r.b.x - 4 * r.a.x * (r.c.x + max.x))) / (2.0 * r.a.x);
+	  double t_max_x_2 = (r.b.x - sqrt(r.b.x*r.b.x - 4 * r.a.x * (r.c.x + max.x))) / (2.0 * r.a.x);
+	  double t_min_x = std::min(t_min_x_1, t_min_x_2);
+	  double t_max_x = std::min(t_max_x_1, t_max_x_2);
 
-  if (t_min_x > t_max_x) std::swap(t_min_x, t_max_x);
+	  if (t_min_x > t_max_x) std::swap(t_min_x, t_max_x);
 
-  double
-          t_min_y = (min.y - r.o.y) / r.d.y,
-          t_max_y = (max.y - r.o.y) / r.d.y;
+	  double t_min_y_1 = (r.b.y + sqrt(r.b.y*r.b.y - 4 * r.a.y * (r.c.y + min.y))) / (2.0 * r.a.y);
+	  double t_min_y_2 = (r.b.y - sqrt(r.b.y*r.b.y - 4 * r.a.y * (r.c.y + min.y))) / (2.0 * r.a.y);
+	  double t_max_y_1 = (r.b.y + sqrt(r.b.y*r.b.y - 4 * r.a.y * (r.c.y + max.y))) / (2.0 * r.a.y);
+	  double t_max_y_2 = (r.b.y - sqrt(r.b.y*r.b.y - 4 * r.a.y * (r.c.y + max.y))) / (2.0 * r.a.y);
+	  double t_min_y = std::min(t_min_y_1, t_min_y_2);
+	  double t_max_y = std::min(t_max_y_1, t_max_y_2);
 
-  if (t_min_y > t_max_y) std::swap(t_min_y, t_max_y);
-  if ((t_min_x > t_max_y) || (t_min_y > t_max_x)) return false;
-  if (t_min_y > t_min_x) t_min_x = t_min_y;
-  if (t_max_y < t_max_x) t_max_x = t_max_y;
+	  if (t_min_y > t_max_y) std::swap(t_min_y, t_max_y);
+	  if ((t_min_x > t_max_y) || (t_min_y > t_max_x)) return false;
+	  if (t_min_y > t_min_x) t_min_x = t_min_y;
+	  if (t_max_y < t_max_x) t_max_x = t_max_y;
 
-  double
-          t_min_z = (min.z - r.o.z) / r.d.z,
-          t_max_z = (max.z - r.o.z) / r.d.z;
+	  double t_min_z_1 = (r.b.z + sqrt(r.b.z*r.b.z - 4 * r.a.z * (r.c.z + min.z))) / (2.0 * r.a.z);
+	  double t_min_z_2 = (r.b.z - sqrt(r.b.z*r.b.z - 4 * r.a.z * (r.c.z + min.z))) / (2.0 * r.a.z);
+	  double t_max_z_1 = (r.b.z + sqrt(r.b.z*r.b.z - 4 * r.a.z * (r.c.z + max.z))) / (2.0 * r.a.z);
+	  double t_max_z_2 = (r.b.z - sqrt(r.b.z*r.b.z - 4 * r.a.z * (r.c.z + max.z))) / (2.0 * r.a.z);
+	  double t_min_z = std::min(t_min_z_1, t_min_z_2);
+	  double t_max_z = std::min(t_max_z_1, t_max_z_2);
 
-  if (t_min_z > t_max_z) std::swap(t_min_z, t_max_z);
-  if ((t_min_x > t_max_z) || (t_min_z > t_max_x)) return false;
-  if (t_min_z > t_min_x) t_min_x = t_min_z;
-  if (t_max_x > t_max_z) t_max_x = t_max_z;
+	  if (t_min_z > t_max_z) std::swap(t_min_z, t_max_z);
+	  if ((t_min_x > t_max_z) || (t_min_z > t_max_x)) return false;
+	  if (t_min_z > t_min_x) t_min_x = t_min_z;
+	  if (t_max_x > t_max_z) t_max_x = t_max_z;
 
-  double
-          new_t0 = std::max({t_min_x, t_min_y, t_min_z}),
-          new_t1 = std::min({t_max_x, t_max_y, t_max_z});
-  t0 = new_t0;
-  t1 = new_t1;
-  return true;
+	  double
+		  new_t0 = std::max({ t_min_x, t_min_y, t_min_z }),
+		  new_t1 = std::min({ t_max_x, t_max_y, t_max_z });
+	  t0 = new_t0;
+	  t1 = new_t1;
+	  return true;
+  }
+  else {
+
+	  double
+		  t_min_x = (min.x - r.o.x) / r.d.x,
+		  t_max_x = (max.x - r.o.x) / r.d.x;
+
+	  if (t_min_x > t_max_x) std::swap(t_min_x, t_max_x);
+
+	  double
+		  t_min_y = (min.y - r.o.y) / r.d.y,
+		  t_max_y = (max.y - r.o.y) / r.d.y;
+
+	  if (t_min_y > t_max_y) std::swap(t_min_y, t_max_y);
+	  if ((t_min_x > t_max_y) || (t_min_y > t_max_x)) return false;
+	  if (t_min_y > t_min_x) t_min_x = t_min_y;
+	  if (t_max_y < t_max_x) t_max_x = t_max_y;
+
+	  double
+		  t_min_z = (min.z - r.o.z) / r.d.z,
+		  t_max_z = (max.z - r.o.z) / r.d.z;
+
+	  if (t_min_z > t_max_z) std::swap(t_min_z, t_max_z);
+	  if ((t_min_x > t_max_z) || (t_min_z > t_max_x)) return false;
+	  if (t_min_z > t_min_x) t_min_x = t_min_z;
+	  if (t_max_x > t_max_z) t_max_x = t_max_z;
+
+	  double
+		  new_t0 = std::max({ t_min_x, t_min_y, t_min_z }),
+		  new_t1 = std::min({ t_max_x, t_max_y, t_max_z });
+	  t0 = new_t0;
+	  t1 = new_t1;
+	  return true;
+  }
 }
 
 void BBox::draw(Color c, float alpha) const {
